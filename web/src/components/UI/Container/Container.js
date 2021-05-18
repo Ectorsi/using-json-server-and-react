999c0510439a7123e88074644fb1a2dfacd316ef
Jose Ector <ector-si@hotmail.com>
2021-05-17 22:37:14 -0300

    Implementing-promotional-search

---
+++ web/src/components/UI/Container/Container.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Container.css'
+const UIContainer = ({children}) => (
+    <div className="UI-container" >
+    {children}
+    </div>
+)
+
+export default UIContainer;
